4d3582d4817288c0f822f13e0cdd62efa29b078f
xuegao <hubeixuegao@gmail.com>
2024-06-18 19:35:58 +0800

    web3 btc

--- web3/src/main.test.ts
+++ web3/src/main.test.ts
@@ -1,14 +1,30 @@
-import addNumbers from "./main";
+import {mnemonicToSeed} from "./bip";
+import {createBtcAddress, importBtcAddress, ValidNetwork} from "./main";
 
-
-test('the data is peanut butter', () => {
-    expect(1).toBe(1)
+test('btc createAddress', () => {
+    const mnemonic = "lounge face pattern cinnamon shrug average spend rapid field cheese wrist weather";
+    const params_1 = {
+        mnemonic: mnemonic,
+        password: ""
+    }
+    const seed = mnemonicToSeed(params_1)
+    const account = createBtcAddress(seed.toString("hex"), "0", "0", "bitcoin")
+    console.log(account)
+    // {
+    //     privateKey: '67fe93913d415ebb15cf6d86cc913efb0b3d724caa743de442cb795481495429',
+    //     publicKey: '02ef67f85c8376cf609a494af8c3a043df98211dec573cf1b0eb17304439cab90d',
+    //     address: '1CzkhKbqwmDL4o8StBdttLNesLpDZpddmA'
+    // }
 });
 
-test('add', () => {
-    let number = addNumbers(1,2);
-    console.log("add ", number)
+test('importBtcAddress', () => {
+    const params = {
+        privateKey: "67fe93913d415ebb15cf6d86cc913efb0b3d724caa743de442cb795481495429",
+        network: "testnet" as ValidNetwork
+    }
+    const account = importBtcAddress(params)
+    // 1CzkhKbqwmDL4o8StBdttLNesLpDZpddmA
+    console.log(account)
 });
-
 
 
--- web3/src/main.ts
+++ web3/src/main.ts
@@ -1,11 +1,66 @@
+import * as ecc from 'tiny-secp256k1';
+import {BIP32API, BIP32Factory} from 'bip32';
+import * as bitcoin from 'bitcoinjs-lib';
+import * as bitcore from 'bitcore-lib';
+// 确保 ecc 实例被正确传递给 BIP32Factory
+const bip32: BIP32API = BIP32Factory(ecc);
 
-// 定义一个方法来计算两个数字的和
-function addNumbers(a: number, b: number): number {
-    return a + b;
+/**
+ *
+ * receiveOrChange 如果 receiveOrChange 为 '1'，则路径改为找零地址路径
+ * */
+export function createBtcAddress(seedHex: string, receiveOrChange: '0' | '1', addressIndex: string, network: keyof typeof bitcoin.networks): {
+    privateKey: string;
+    publicKey: string;
+    address: string | undefined
+} {
+    const root = bip32.fromSeed(Buffer.from(seedHex, 'hex'));
+    let path = "m/44'/0'/0'/0/" + addressIndex + '';
+    if (receiveOrChange === '1') {
+        // 如果 receiveOrChange 为 '1'，则路径改为找零地址路径
+        path = "m/44'/0'/0'/1/" + addressIndex + '';
+    }
+    const child = root.derivePath(path);
+
+    // 生成地址
+    const {address} = bitcoin.payments.p2pkh({
+        pubkey: child.publicKey,
+        network: bitcoin.networks[network]
+    });
+    return {
+        privateKey: child.privateKey ? child.privateKey.toString('hex') : '',
+        publicKey: child.publicKey.toString('hex'),
+        address
+    };
 }
 
-export default addNumbers;
+// 定义有效的网络名称类型
+export type ValidNetwork = 'livenet' | 'testnet' | 'regtest'; // 根据实际需要添加更多网络名称
+/**
+ * import address
+ * private key
+ * network
+ * @param params
+ */
+export function importBtcAddress(params: { privateKey: string; network: ValidNetwork }) {
+    const {privateKey, network} = params;
+    const net = bitcore.Networks.get(network, []);
+    // 检查私钥是否有效
+    // if (!bitcore.PrivateKey.isValid(privateKey, net)) {
+    //     throw new Error('PrivateKey is not valid.');
+    // }
+    // 使用私钥生成地址
+    const privateKeyObj = new bitcore.PrivateKey(privateKey, net);
+    const address = privateKeyObj.toAddress().toString();
+    return address;
+}
 
+
+export function verifyBtcAddress (params: { address: string; network: ValidNetwork }) {
+    const { address, network } = params;
+    const net = bitcore.Networks.get(network, []);
+    return bitcore.Address.isValid(address, net);
+}
 
 
 
