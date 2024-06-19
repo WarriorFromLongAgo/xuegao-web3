dd2fa05697ced8f45555ed5383ca14cee424b0d9
xuegao <hubeixuegao@gmail.com>
2024-06-19 20:50:36 +0800

    web3 btc

--- web3/src/main.test.ts
+++ web3/src/main.test.ts
@@ -1,5 +1,5 @@
 import {mnemonicToSeed} from "./bip";
-import {createBtcAddress, importBtcAddress, ValidNetwork} from "./main";
+import {createBtcAddress, importBtcAddress, signBtcTransaction, SignObj, ValidNetwork, verifyBtcAddress} from "./main";
 
 test('btc createAddress', () => {
     const mnemonic = "lounge face pattern cinnamon shrug average spend rapid field cheese wrist weather";
@@ -19,12 +19,51 @@ test('btc createAddress', () => {
 
 test('importBtcAddress', () => {
     const params = {
-        privateKey: "67fe93913d415ebb15cf6d86cc913efb0b3d724caa743de442cb795481495429",
-        network: "testnet" as ValidNetwork
+        // privateKey: "67fe93913d415ebb15cf6d86cc913efb0b3d724caa743de442cb795481495429",
+        privateKey: "67fe93913d415ebb15cf6d86cc913efb0b3d724caa743de442cb79548149542911",
+        network: "mainnet" as ValidNetwork
     }
     const account = importBtcAddress(params)
     // 1CzkhKbqwmDL4o8StBdttLNesLpDZpddmA
     console.log(account)
 });
+
+
+test('verifyBtcAddress', () => {
+    const params = {
+        address: "1CzkhKbqwmDL4o8StBdttLNesLpDZpddmA",
+        network: "mainnet" as ValidNetwork
+    }
+    const flag = verifyBtcAddress(params)
+    // 1CzkhKbqwmDL4o8StBdttLNesLpDZpddmA
+    console.log(flag)
+});
+
+
+test('sign', () => {
+    const data: SignObj = {
+        inputs: [
+            {
+                address: "1H1oAqmdfTNECrrHFAJ4AhbTUyPcQjrf72",
+                txid: "209706b97a9aed047df158bf57cfbdad94a5e9bd9ac5261034448ec4590bab8f",
+                amount: 9000000000000000,
+                vout: 0,
+            },
+        ],
+        outputs: [
+            {
+                amount: 9000000000000000,
+                address: "1H1oAqmdfTNECrrHFAJ4AhbTUyPcQjrf72",
+            },
+        ],
+    };
+    const rawHex = signBtcTransaction({
+        privateKey: "60164bec9512d004af7f71e7ed868c8e9ac2cc6234d8b682037ec80547595f2e",
+        signObj: data,
+        network: "mainnet"
+    });
+    console.log(rawHex);
+});
+
 
 
--- web3/src/main.ts
+++ web3/src/main.ts
@@ -1,7 +1,8 @@
 import * as ecc from 'tiny-secp256k1';
 import {BIP32API, BIP32Factory} from 'bip32';
 import * as bitcoin from 'bitcoinjs-lib';
-import * as bitcore from 'bitcore-lib';
+
+const bitcore: any = require('bitcore-lib');
 // 确保 ecc 实例被正确传递给 BIP32Factory
 const bip32: BIP32API = BIP32Factory(ecc);
 
@@ -35,7 +36,7 @@ export function createBtcAddress(seedHex: string, receiveOrChange: '0' | '1', ad
 }
 
 // 定义有效的网络名称类型
-export type ValidNetwork = 'livenet' | 'testnet' | 'regtest'; // 根据实际需要添加更多网络名称
+export type ValidNetwork = 'mainnet' | 'testnet' | 'livenet'; // 根据实际需要添加更多网络名称
 /**
  * import address
  * private key
@@ -44,24 +45,81 @@ export type ValidNetwork = 'livenet' | 'testnet' | 'regtest'; // 根据实际需
  */
 export function importBtcAddress(params: { privateKey: string; network: ValidNetwork }) {
     const {privateKey, network} = params;
-    const net = bitcore.Networks.get(network, []);
+    // 获取网络对象
+    const net = bitcore.Networks[network];
+    if (!net) {
+        throw new Error(`Invalid network: ${network}`);
+    }
     // 检查私钥是否有效
-    // if (!bitcore.PrivateKey.isValid(privateKey, net)) {
-    //     throw new Error('PrivateKey is not valid.');
-    // }
-    // 使用私钥生成地址
+    if (!bitcore.PrivateKey.isValid(privateKey, network)) {
+        throw new Error('PrivateKey is not valid.');
+    }
+
+    // 使用私钥生成私钥对象
     const privateKeyObj = new bitcore.PrivateKey(privateKey, net);
+
+    // 使用私钥对象生成地址
     const address = privateKeyObj.toAddress().toString();
+
     return address;
 }
 
 
-export function verifyBtcAddress (params: { address: string; network: ValidNetwork }) {
-    const { address, network } = params;
-    const net = bitcore.Networks.get(network, []);
-    return bitcore.Address.isValid(address, net);
+export function verifyBtcAddress(params: { address: string; network: ValidNetwork }) {
+    const {address, network} = params;
+    // 获取网络对象
+    const net = bitcore.Networks[network];
+    if (!net) {
+        throw new Error(`Invalid network: ${network}`);
+    }
+
+    return bitcore.Address.isValid(address, net, 'script');
 }
 
+// 定义 SignObj 接口
+export interface SignObj {
+    inputs: Array<{
+        address: string;
+        txid: string;
+        amount: number;
+        vout: number;
+    }>;
+    outputs: Array<{
+        address: string;
+        amount: number;
+    }>;
+}
+
+/**
+ * 暂不支持taproot签名
+ * @returns
+ * @param params
+ */
+export function signBtcTransaction(params: { privateKey: string; signObj: SignObj; network: ValidNetwork; }): string {
+    const {privateKey, signObj, network} = params;
+    // 获取网络对象
+    const net = bitcore.Networks[network];
+
+    const inputs = signObj.inputs.map(input => {
+        return {
+            address: input.address,
+            txId: input.txid,
+            outputIndex: input.vout,
+            script: new bitcore.Script.fromAddress(input.address).toHex(),
+            satoshis: input.amount
+        }
+    });
+    const outputs = signObj.outputs.map(output => {
+        return {
+            address: output.address,
+            satoshis: output.amount
+        };
+    });
+    const transaction = new bitcore.Transaction(net).from(inputs).to(outputs);
+    transaction.version = 2;
+    transaction.sign(privateKey);
+    return transaction.toString();
+}
 
 
 
