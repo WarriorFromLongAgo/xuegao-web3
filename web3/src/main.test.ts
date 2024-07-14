b741a520fad3f5df87b566885e2e868882edfc8b
xuegao <hubeixuegao@gmail.com>
2024-07-14 18:04:22 +0800

    ton

--- web3/src/main.test.ts
+++ web3/src/main.test.ts
@@ -1,14 +1,31 @@
-import addNumbers from "./main";
+import bip39 = require('bip39');
+import {createTonAddress, SignTransaction} from "./main";
 
 
-test('the data is peanut butter', () => {
-    expect(1).toBe(1)
+test('create address', async () => {
+    const mnemonic = "champion junior glimpse analyst plug jump entire barrel slight swim hidden remove";
+
+    // 这里添加了密码参数
+    const seed = bip39.mnemonicToSeedSync(mnemonic, "")
+    // 假设这是您定义的函数
+    const addressInfo = await createTonAddress(seed.toString(), 0)
+    console.log("addressInfo===", addressInfo)
 });
 
-test('add', () => {
-    let number = addNumbers(1,2);
-    console.log("add ", number)
-});
+
+test('sign transaction', async () => {
+    const param = {
+        from: "UQAUAHcUab66DpOV2GaT_QDuSagpMdIn0x6aMmO3_fPVM305",
+        to: "EQCQCLTvR0XYTyM0uxh_H8kLAR7u7v98pEKZKpbq8w2peuNY",
+        memo: "memo",
+        amount: 0.01,
+        sequence: 38103804,
+        decimal: 10,
+        privateKey: "b0e4eb37bc5929491899d2a50f52f0a4613d3a48e56245267fdecff392ead89b7e4fdf79bf78566b85b73787e5739ab4306350d7ad1adc50be9c57fe2102bfcc"
+    }
+    const sign_message = await SignTransaction(param)
+    console.log("sign_message===", sign_message)
+})
 
 
 
--- web3/src/main.ts
+++ web3/src/main.ts
@@ -1,12 +1,78 @@
+import {derivePath, getPublicKey} from "ed25519-hd-key";
+import TonWeb from "tonweb";
+import BigNumber from "bignumber.js";
 
-// 定义一个方法来计算两个数字的和
-function addNumbers(a: number, b: number): number {
-    return a + b;
+// console.log
+// addressInfo=== {
+//     privateKey: '1079e489e04925bc3207db41ab0d4341c9f3c7b808078c9a8320e5e2f3471db22dc315b7ba636eec21527be1f2edfac287addb235c1eb71a70300c1208d30f9c',
+//     publicKey: '2dc315b7ba636eec21527be1f2edfac287addb235c1eb71a70300c1208d30f9c',
+//     address: 'EQA5PEhPzGZgmMOk9Fr2sPEAY03HLueelXFXFWdwKxh0h7Pq'
+// }
+
+export async function createTonAddress(seedHex: string, addressIndex: number) {
+    const {key} = derivePath("m/44'/607'/1'/" + addressIndex + "'", seedHex);
+    const publicKey = getPublicKey(key, false).toString('hex');
+
+    const tonweb = new TonWeb();
+    const WalletClass = tonweb.wallet.all['v3R2'];
+
+    const pubKey = new Uint8Array(Buffer.from(publicKey, "hex"))
+    const wallet = new WalletClass(tonweb.provider, {
+        publicKey: pubKey,
+        wc: 0
+    });
+    const walletAddress = await wallet.getAddress();
+    return {
+        "privateKey": key.toString('hex') + publicKey,
+        "publicKey": publicKey,
+        "address": walletAddress.toString(true, true, true, false)
+    }
 }
 
-export default addNumbers;
 
+export async function SignTransaction(params: {
+    from: string;
+    to: string;
+    memo: string;
+    amount: number;
+    sequence: number;
+    decimal: number;
+    privateKey: string;
+}) {
+    const {from, to, memo, amount, sequence, decimal, privateKey} = params;
 
+    const tonweb = new TonWeb();
 
+    const calcAmount = new BigNumber(amount).times(new BigNumber(10).pow(decimal)).toNumber();
+
+    if (calcAmount % 1 !== 0) throw new Error("amount invalid");
+    const keyPair = TonWeb.utils.nacl.sign.keyPair.fromSecretKey(new Uint8Array(Buffer.from(privateKey, 'hex')));
+    const WalletClass = tonweb.wallet.all['v4R2'];
+    const wallet = new WalletClass(tonweb.provider, {
+        publicKey: keyPair.publicKey,
+        wc: 0
+    });
+    let secretKey = keyPair.secretKey
+    const walletAddress = await wallet.getAddress();
+    console.log(walletAddress.toString(true, true, false, false))
+    const fromAddres = walletAddress.toString(true, true, false, false);
+    if (from !== fromAddres) throw new Error("from address invalid");
+    const toAddress = new TonWeb.utils.Address(to);
+    const tx_ret = wallet.methods.transfer({
+        secretKey,
+        toAddress: toAddress.toString(true, true, false, false),
+        amount: calcAmount,
+        seqno: sequence,
+        payload: memo || "",
+        sendMode: 3, // 3 默认为转账, 细节请看调研文档
+    })
+    const queryData = await tx_ret.getQuery()
+    const hash = await queryData.hash()
+    const boc = await queryData.toBoc(false);
+    return {
+        "hash": TonWeb.utils.bytesToBase64(hash),
+        "rawtx": TonWeb.utils.bytesToBase64(boc)
+    }
+}
 
 
