18b8dc84e4aa538289d763a96ede33b69cc0ce7f
xuegao <hubeixuegao@gmail.com>
2024-10-11 16:52:47 +0800

    erc721

--- src/NewNftManager.sol
+++ src/NewNftManager.sol
@@ -2,26 +2,28 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract NewNftManager is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract NewNftManager is Initializable, ERC721Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using Strings for uint256;
 
     uint256 private _nextTokenId;
     mapping(uint256 => uint8) public nftMintType;
 
-    string public constant BASIC_NFT_URL = "https://www.fishcake.io/_next/image?url=%2Ficons%2Ffishcake%2Fnft-basic-create.png&w=256&q=75";
-    string public constant PRO_NFT_URL = "https://www.fishcake.io/_next/image?url=%2Ficons%2Ffishcake%2Fnft-pro-create.png&w=256&q=75";
+    string public basicNftImage;
+    string public proNftImage;
 
     event CreateNFT(
         address indexed creator,
         uint256 tokenId,
         uint8 nftType
     );
+
+    event UpdatedNftImage(uint8 nftType, string newImageUrl);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -31,9 +33,11 @@ contract NewNftManager is Initializable, ERC721Upgradeable, ERC721URIStorageUpgr
     function initialize(address _initialOwner) public initializer {
         require(_initialOwner != address(0), "NewNftManager initialize: _initialOwner can't be zero address");
         __ERC721_init("Fishcake Pass NFT", "FNFT");
-        __ERC721URIStorage_init();
         __Ownable_init(_initialOwner);
         __ReentrancyGuard_init();
+
+        basicNftImage = "https://www.fishcake.io/_next/image?url=%2Ficons%2Ffishcake%2Fnft-basic-create.png&w=256&q=75";
+        proNftImage = "https://www.fishcake.io/_next/image?url=%2Ficons%2Ffishcake%2Fnft-pro-create.png&w=256&q=75";
     }
 
     function createNFT(uint8 _type) external nonReentrant returns (uint256) {
@@ -42,20 +46,46 @@ contract NewNftManager is Initializable, ERC721Upgradeable, ERC721URIStorageUpgr
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
         nftMintType[tokenId] = _type;
-        string memory uri = _type == 1 ? BASIC_NFT_URL : PRO_NFT_URL;
-        _setTokenURI(tokenId, uri);
 
         emit CreateNFT(msg.sender, tokenId, _type);
 
         return tokenId;
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
-        return super.tokenURI(tokenId);
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "ERC721Metadata: URI query for nonexistent token");
+
+        uint8 nftType = nftMintType[tokenId];
+        string memory image = nftType == 1 ? basicNftImage : proNftImage;
+        string memory name = nftType == 1 ? "Fishcake Basic Pass" : "Fishcake Pro Pass";
+        string memory description = nftType == 1 ? "A Fishcake Basic Pass" : "A Fishcake Pro Pass";
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "', name, '", ',
+                        '"description": "', description, '", ',
+                        '"image": "', image, '"}'
+                    )
+                )
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", json));
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
+    function updateNftImage(uint8 _type, string memory _newImageUrl) external onlyOwner {
+        require(_type == 1 || _type == 2, "Invalid NFT type");
+        if (_type == 1) {
+            basicNftImage = _newImageUrl;
+        } else {
+            proNftImage = _newImageUrl;
+        }
+        emit UpdatedNftImage(_type, _newImageUrl);
+    }
 }
